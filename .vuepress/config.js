f0947ae28ecbcb2d912c175e964fff2896cefc3c
Antoine Fontaine <foster@dnd.fr>
2018-08-06 14:10:33 +0200

    all : init

---
+++ .vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: 'My documentation',
+    description: 'Documentation information',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            { text: 'External', link: 'https://google.com' },
+        ]
+    }
+};
